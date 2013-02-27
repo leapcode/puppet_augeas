9a21ea77e55cc7b722c5ae15ba8ae34f89f33e08
mh <mh@immerda.ch>
2013-02-27 23:11:44 +0100

    linting traling whitespaces

--- manifests/lens.pp
+++ manifests/lens.pp
@@ -1,18 +1,18 @@
 # == Definition: augeas::lens
-# 
+#
 # Deploy an Augeas lens (and its test file).
 # Check the lens (and run the unit tests) automatically and remove the files if
 # the checks fail.
-# 
+#
 # Parameters:
 # - *ensure*: present/absent
 # - *lens_source*: the source for the lens
 # - *test_source*: optionally, the source for the test file.
 # - *stock_since*: optionally, indicate in which version of Augeas
 #   the lens became stock, so it will not be deployed above that version.
-# 
+#
 # Example usage:
-# 
+#
 #   augeas::lens { 'networkmanager':
 #     lens_source => 'puppet:///modules/networkmanager/lenses/networkmanager.aug',
 #     test_source => 'puppet:///modules/networkmanager/lenses/test_networkmanager.aug',
